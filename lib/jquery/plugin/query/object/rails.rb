748487f1420003c8eacd72ece39bc100c3404bca
Toshiyuki HINA <thina@yourinventit.com>
2019-02-05 13:33:29 +0900

    initial om

---
+++ lib/jquery/plugin/query/object/rails.rb
@@ -0,0 +1,14 @@
+require "jquery/plugin/query/object/rails/version"
+
+module Jquery
+  module Plugin
+    module Query
+      module Object
+        module Rails
+          class Error < StandardError; end
+          # Your code goes here...
+        end
+      end
+    end
+  end
+end
---
+++ lib/jquery/plugin/query/object/rails/version.rb
@@ -0,0 +1,11 @@
+module Jquery
+  module Plugin
+    module Query
+      module Object
+        module Rails
+          VERSION = "0.1.0"
+        end
+      end
+    end
+  end
+end
